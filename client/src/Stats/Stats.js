46c63523dccb6f862145f1f53fdc296eb0b4e8bd
trungle26 <leductrung8520@gmail.com>
2023-03-31 22:38:09 +0700

    not trang 4

--- client/src/Stats/Stats.js
+++ client/src/Stats/Stats.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./Stats.scss";
 import Navbar from "../Navbar/Navbar";
+import Table from "../Home/Table";
+import data from "../data";
 
 export default function () {
   return (
     <div>
       <Navbar />
+      <div className="stats">
+        <h1 className="stats-header">Xe chưa hết hạn:</h1>
+        <Table className="stats-table" data={data} />
+      </div>
     </div>
   );
 }
