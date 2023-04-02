2dd98162f9dc21908475bf1b5f092044c9b53e70
jeikai <bomchip02@gmail.com>
2023-04-02 21:21:30 +0700

    sua chut it

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import Login from './Login/Login'
+import Navbar from './Navbar/Navbar'
+import Home from './Home/Home';
 function App() {
+  const [login, setLogin] = useState({
+    user: localStorage.getItem('user')
+  })
   return (
     <>
-      <h1>Hello</h1>
+      {/* <Routes>
+        <Route exact path='/' element={<Login/>}></Route>
+      </Routes> */}
+      <Navbar/>
+      <Routes>
+        <Route exact path='/' element={<Home/>}></Route>
+      </Routes>
     </>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
-import Login from "./Login/Login";
-import Navbar from "./Navbar/Navbar";
-import Home from "./Home/Home";
-import Form from "./Form/Form";
-import Stats from "./Stats/Stats";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Stats />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
+
