6ef7f91937df9b128d5f05e98509b116036218bb
jeikai <bomchip02@gmail.com>
2023-04-03 08:44:15 +0700

    sua UI

--- client/src/Login/Login.js
+++ client/src/Login/Login.js
@@ -3,16 +3,27 @@ import "./Login.scss";
 
 export default function Login() {
   return (
-    <div id="login">
-      <form id="login-panel">
-        <h1>CỤC ĐĂNG KIỂM VIỆT NAM</h1>
-        <img src="./img/logo.png" />
-        <input type="text" id="username" placeholder="Tên đăng nhập"></input>
-        <input type="password" id="password" placeholder="Mật khẩu"></input>
-        <button type="button" id="login-button">
-          Đăng nhập
-        </button>
-      </form>
-    </div>
+    <section>
+      <div className="login">
+        <form action="">
+          <h2>CỤC ĐĂNG KIỂM VIỆT NAM</h2>
+          <img src="./img/logo.png" />
+          <div className="inputbox">
+            <i class="fa-regular fa-user"></i>
+            <input type="text" name="username" id="username"></input>
+            <label for="username">Tên đăng nhập</label>
+          </div>
+
+          <div className="inputbox">
+            <i class="fa-solid fa-lock"></i>
+            <input type="password" name="password" id="password" ></input>
+            <label for="password">Mật khẩu</label>
+          </div>
+          <button type="button" id="login-button">
+            Đăng nhập
+          </button>
+        </form>
+      </div>
+    </section>
   );
 }
