bce712b87abe49b438021b0bab7dd701b5893d52
trungle26 <leductrung8520@gmail.com>
2023-04-10 20:49:33 +0700

    chinh lai giao dien + lam them home cho admin

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
-import { BrowserRouter } from 'react-router-dom';
+import HomeAdmin from "./HomeAdmin/HomeAdmin";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <div>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    <HomeAdmin />
+  </div>
 );
-
-
